5f1849bffa233198c9b11be2c65e5b017c04e405
jonathan4122 <109387322+jonathan4122@users.noreply.github.com>
2022-11-09 16:27:18 -0600

    Update cypress.config.js

--- cypress.config.js
+++ cypress.config.js
@@ -1,15 +1,14 @@
 const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   video: true,
+  projectId: "gwkjzq",
   e2e: {
     "demohost_url": "https://demoqa.ohmyfi.com/demo/iniciodemo?token=b152dae0f7474a61237add342afe9ea1be93cca187e923aabc702667ba5f0d06&lugar=4089",
     "num_Phone": "35648978",
     "Token_test": "12345",
     "Name_User": "BootUserTest",
-    "projectId": "gwkjzq",
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
-  },
-  "projectId": "gwkjzq"
+  }
 });

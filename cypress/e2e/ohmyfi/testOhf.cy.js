e99a5db5a151cb75d8bff0e1d24fb6499b696a46
Jonathan_Castro <j.castro@ohmyfi.com>
2022-11-09 15:08:15 -0600

    na333333sasdasasdfasdaaa

--- cypress/e2e/ohmyfi/testOhf.cy.js
+++ cypress/e2e/ohmyfi/testOhf.cy.js
@@ -1,7 +1,8 @@
 describe('Primer prueba de portales', () => {
     beforeEach(() => {
         cy.visit("https://google.com");
-    });
+    })
     it('relleno de formulario!', () => {
-    });
+        cy.window().click()
+    })
 })

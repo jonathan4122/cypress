190f5f36129a81907d4d698c64668e4702134cb2
Jonathan_Castro <j.castro@ohmyfi.com>
2022-11-09 14:22:48 -0600

    na333333ss32

--- cypress.config.js
+++ cypress.config.js
@@ -1,7 +1,12 @@
 const { defineConfig } = require("cypress");
-
 module.exports = defineConfig({
+  video: true,
   e2e: {
+    "demohost_url": "https://demoqa.ohmyfi.com/demo/iniciodemo?token=b152dae0f7474a61237add342afe9ea1be93cca187e923aabc702667ba5f0d06&lugar=4089",
+    "num_Phone": "35648978",
+    "Token_test": "12345",
+    "Name_User": "BootUserTest",
+    "projectId": "imix9s",
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },

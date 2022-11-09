908fb997d61ee30d6663a99273f9bb1efeffa0d6
Jonathan_Castro <j.castro@ohmyfi.com>
2022-11-09 15:43:01 -0600

    llsssl

--- cypress.config.js
+++ cypress.config.js
@@ -2,7 +2,7 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   video: true,
   e2e: {
-    "demohost_url": "https://demoqa.ohmyfi.com/demo/iniciodemo?token=b152dae0f7474a61237add342afe9ea1be93cca187e923aabc702667ba5f0d06&lugar=4089",
+    "demohost_url": "https://demoqa.ohmyfi.com/demo/iniciodemo?token=b152dae0f7474a61237add342afba5f0d06&lugar=4089",
     "num_Phone": "35648978",
     "Token_test": "12345",
     "Name_User": "BootUserTest",

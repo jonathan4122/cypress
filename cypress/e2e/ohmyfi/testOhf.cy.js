660b476b50cbc2d170e7083a76e5638377a63932
Jonathan_Castro <j.castro@ohmyfi.com>
2022-11-23 17:19:28 -0500

    ajuste cypress 2

--- cypress/e2e/ohmyfi/testOhf.cy.js
+++ cypress/e2e/ohmyfi/testOhf.cy.js
@@ -1,9 +1,6 @@
 describe('Test Login Plataforma', () => {
-    beforeEach(() => {
-        cy.visit("https://ohmyfi.com/clientes");
-    })
     it('relleno de formulario!', () => {
-        cy.get('input[name="usuario"]').type('hola') // Click on buttonaaaaaaa
-        cy.get('input[name="clave"]').type('111111') // Click on buttonaaaaaaa
+        cy.visit("https://google.com/");
+        cy.get('.gLFyf').type('hola') // Click on buttonaaaaaaa
     })
 })

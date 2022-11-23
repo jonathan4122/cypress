949567211d57012194d42ce817b05dec9f70fcdf
Jonathan_Castro <j.castro@ohmyfi.com>
2022-11-23 17:41:48 -0500

    Prueba Prr

--- cypress.config.js
+++ cypress.config.js
@@ -2,7 +2,7 @@ module.exports = {
   projectId: "zyd4np",
   e2e: {
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      // implement node event listeners
     },
   }
 }

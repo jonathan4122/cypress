30fa0da2df7f35b0769e06a5c675b2c48a5cd7ef
Jonathan_Castro <j.castro@ohmyfi.com>
2022-11-23 17:25:51 -0500

    Prueba PR dash

--- cypress/e2e/ohmyfi/testOhf.cy.js
+++ cypress/e2e/ohmyfi/testOhf.cy.js
@@ -1,6 +1,6 @@
 describe('Test Login Plataforma', () => {
     it('relleno de formulario!', () => {
         cy.visit("https://google.com/");
-        cy.get('.gLFyf').type('hola') // Click on buttonaaaaaaa
+        cy.get('.gLFyf').type('hola') // Click on button
     })
 })

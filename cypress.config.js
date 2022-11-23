86bee81dc1c6ca5bb509fcc67663ad519c143dfc
Jonathan_Castro <j.castro@ohmyfi.com>
2022-11-23 17:06:27 -0500

    ajuste cypress

--- cypress.config.js
+++ cypress.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  projectId: "gwkjzq",
+  projectId: "zyd4np",
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
--- cypress/e2e/ohmyfi/testOhf.cy.js
+++ cypress/e2e/ohmyfi/testOhf.cy.js
@@ -1,8 +1,9 @@
-describe('Primer prueba de portales', () => {
+describe('Test Login Plataforma', () => {
     beforeEach(() => {
         cy.visit("https://ohmyfi.com/clientes");
     })
     it('relleno de formulario!', () => {
-        cy.get('.gLFyf').type('hola') // Click on buttonaaaaaaa
+        cy.get('input[name="usuario"]').type('hola') // Click on buttonaaaaaaa
+        cy.get('input[name="clave"]').type('111111') // Click on buttonaaaaaaa
     })
 })

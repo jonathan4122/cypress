4b4db7a77d3f354d76cef5312275797032edc72a
Jonathan_Castro <j.castro@ohmyfi.com>
2022-12-01 11:22:06 -0500

    Cambio Jonathan

--- cypress/e2e/ohmyfi/testOhf.cy.js
+++ cypress/e2e/ohmyfi/testOhf.cy.js
@@ -1,6 +1,6 @@
 describe('Test Login Plataforma', () => {
     it('relleno de formulario!', () => {
         cy.visit("https://google.com/");
-        cy.get('.gLFyf').type('hola') // Click on
+        cy.get('.gLFyf').type('hola') // Click
     })
 })
